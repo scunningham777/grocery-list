4e2187a7984990ed1dd995658250be74be5c35fd
s.cunningham777@gmail.com <s.cunningham777@gmail.com>
2017-05-17 07:52:00 -0500

    List item categories; listitem swipe & slide buttons

--- src/pages/grocery-list/grocery-list.ts
+++ src/pages/grocery-list/grocery-list.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavParams, ModalController } from 'ionic-angular';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/reduce';
 
 import { ListService } from '../../providers/list-service';
 import { ListItemService } from '../../providers/list-item-service';
@@ -14,6 +15,7 @@ import { List, ListItem } from '../../models/models';
 export class GroceryListPage {
     public list: List;
     public listItems$: Observable<ListItem[]>;
+    public listItemsByCategory$: Observable<any[]>;
 
     constructor(
         public navParams: NavParams,
@@ -27,6 +29,26 @@ export class GroceryListPage {
             });
 
         this.listItems$ = itemSvc.getAllListItemsForList(navParams.get('listId'));
+        this.listItemsByCategory$ = this.listItems$
+            .map((listItems: ListItem[]) => {
+                return listItems.reduce((acc, listItem: ListItem) => {
+                    if (acc.find(cat => cat.name == listItem.categoryName) === undefined) {
+                        acc.push({
+                            name: listItem.categoryName,
+                            items: []
+                        });
+                    }
+                    acc.find(cat => cat.name == listItem.categoryName).items.push(listItem);
+                    return acc;
+                }, [])
+                .sort((a, b) => {
+                    if (a.name == null) return 1;
+                    if (b.name == null) return -1;
+                    else {
+                        return a.name.localeCompare(b.name);
+                    }
+                })
+            })
     }
 
     ionViewDidLoad() {
@@ -37,4 +59,18 @@ export class GroceryListPage {
         modal.present();
     }
 
+    checkCompletedSwipe(item, listItemId) {
+        const slidePercent = item.getSlidingPercent()
+        if (slidePercent < 0) {
+            if (slidePercent < -2) {
+                this.completeItem(listItemId);
+            } else {
+                console.dir(item);
+            }
+        }
+    }
+
+    completeItem(listItemId: string) {
+        console.log('Item completed: ', listItemId);
+    }
 }
